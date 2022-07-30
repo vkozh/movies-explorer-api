ba91e8e0442753b2644fa6d567e1632694affdf7
Lekozhe <v.a.kozh@ya.ru>
2022-07-30 09:38:10 +0300

    fix: movieId to wtring

--- models/movie.js
+++ models/movie.js
@@ -42,7 +42,7 @@ const movieSchema = new mongoose.Schema({
     required: true,
   },
   movieId: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: String,
     required: true,
   },
   nameRU: {
